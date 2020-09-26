4cb960924ff67558a732e0358ad1c12b51a826e9
Kirk Hahn <kirkhahn11@yahoo.com>
2020-09-26 13:48:57 -0500

    Completed User Can Flip Cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+var gameCards = document.getElementById('gameCards')
+console.log(gameCards)
+gameCards.addEventListener('click', handleClick);
 
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  var hidden = event.target
+  hidden.className = "hidden"
+}
